66b9df15de33b00f824d8217d3fd35e65e231e33
huanthuytnhh <huanthuytnhh@gmail.com>
2024-03-21 09:06:22 +0700

    Finish BT2

--- BT2/BT2/Form1.Designer.cs
+++ BT2/BT2/Form1.Designer.cs
@@ -28,13 +28,146 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Browse = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(325, 170);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(193, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load Treeview";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(26, 130);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(272, 337);
+            this.treeView1.TabIndex = 1;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(325, 216);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(193, 40);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Add Node";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(325, 262);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(193, 40);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Delete Node";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(325, 308);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(193, 40);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Edit Node";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(325, 354);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(193, 40);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Save Treeview";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(325, 400);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(193, 40);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Find Node";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(82, 50);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(236, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // Browse
+            // 
+            this.Browse.Location = new System.Drawing.Point(347, 50);
+            this.Browse.Name = "Browse";
+            this.Browse.Size = new System.Drawing.Size(107, 20);
+            this.Browse.TabIndex = 10;
+            this.Browse.Text = "Browse";
+            this.Browse.UseVisualStyleBackColor = true;
+            this.Browse.Click += new System.EventHandler(this.button0_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(542, 493);
+            this.Controls.Add(this.Browse);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "BT2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button Browse;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- BT2/BT2/Form1.cs
+++ BT2/BT2/Form1.cs
@@ -1,20 +1,291 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
 using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace BT2
 {
     public partial class Form1 : Form
     {
+        private XmlDocument doc;
+        private TreeNode selectedNode;
+
         public Form1()
         {
             InitializeComponent();
+        }
+        private string filePath;
+        private void button0_Click(object sender, EventArgs e) // Browse_Click
+        {
+            OpenFileDialog o = new OpenFileDialog();
+            if (o.ShowDialog()==DialogResult.OK)
+            {
+                textBox2.Text= o.FileName;
+                filePath= o.FileName;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e) // Load_click
+        {
+            doc = new XmlDocument();
+            try
+            {
+                doc.Load(filePath);
+                RefreshTreeView();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading XML: {ex.Message}");
+            }
+        }
+
+        private void RefreshTreeView()
+        {
+            treeView1.Nodes.Clear();
+            if (doc.DocumentElement != null)
+            {
+                treeView1.Nodes.Add(CreateTreeNode(doc.DocumentElement));
+            }
+        }
+
+        private TreeNode CreateTreeNode(XmlNode xmlNode)
+        {
+            TreeNode treeNode = new TreeNode(xmlNode.Name);
+            foreach (XmlAttribute attribute in xmlNode.Attributes)
+            {
+                treeNode.Nodes.Add($"{attribute.Name}: {attribute.Value}");
+            }
+
+            foreach (XmlNode node in xmlNode.ChildNodes)
+            {
+                if (node.NodeType == XmlNodeType.Element)
+                {
+                    treeNode.Nodes.Add(CreateTreeNode(node));
+                }
+                else if (node.NodeType == XmlNodeType.Text)
+                {
+                    treeNode.Text = node.Value;
+                }
+            }
+            return treeNode;
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            selectedNode = e.Node;
+        }
+
+        private TextBox dynamicTextBox; 
+
+        private void button4_Click(object sender, EventArgs e) // Edit_click
+        {
+            if (selectedNode != null)
+            {
+                dynamicTextBox = new TextBox
+                {
+                    Location = new Point(325, 130),
+                    Size = new Size(193, 20),
+                    Text = selectedNode.Text
+                };
+                dynamicTextBox.KeyPress += TextBox_KeyPress; 
+                dynamicTextBox.Focus();
+
+                this.Controls.Add(dynamicTextBox);
+            }
+            else
+            {
+                MessageBox.Show("Please select a node to edit.");
+            }
+        }
+        private void button6_Click(object sender, EventArgs e) //Find_click
+        {
+            dynamicTextBox = new TextBox
+            {
+                Location = new Point(325, 130),
+                Size = new Size(193, 20),
+                Visible = true
+            };
+
+            this.Controls.Add(dynamicTextBox);
+            dynamicTextBox.Focus();
+
+            dynamicTextBox.KeyPress += (s, keyPressEventArgs) =>
+            {
+                if (keyPressEventArgs.KeyChar == (char)Keys.Enter)
+                {
+                    // Use the text from the TextBox for searching
+                    string partialNameToFind = dynamicTextBox.Text.Trim();
+                    TreeNode foundNode = FindNodeByPartialName(treeView1.Nodes, partialNameToFind);
+
+                    if (foundNode != null)
+                    {
+                        treeView1.SelectedNode = foundNode;
+                        treeView1.Focus();
+                    }
+                    else
+                    {
+                        MessageBox.Show("No node with the specified partial name found.");
+                    }
+
+                    dynamicTextBox.Clear();
+                    dynamicTextBox.Visible = false;
+                    keyPressEventArgs.Handled = true; 
+                }
+            };
+        }
+
+        private TreeNode FindNodeByPartialName(TreeNodeCollection nodes, string partialName)
+        {
+            foreach (TreeNode node in nodes)
+            {
+                if (node.Text.IndexOf(partialName, StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    return node; 
+                }
+
+                TreeNode foundNode = FindNodeByPartialName(node.Nodes, partialName);
+                if (foundNode != null)
+                {
+                    return foundNode; 
+                }
+            }
+            return null; 
+        }
+        private void TextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter) 
+            {
+                string newValue = dynamicTextBox.Text.Trim();
+                if (!string.IsNullOrEmpty(newValue))
+                {
+                    selectedNode.Text = newValue;
+                    UpdateXmlNodeFromTreeNode(selectedNode, newValue);
+                    dynamicTextBox.Visible = false;
+                }
+            }
+        }
+
+        private void UpdateXmlNodeFromTreeNode(TreeNode treeNode, string newValue)
+        {
+            XmlNode xmlNode = FindXmlNode(treeNode, doc.DocumentElement);
+            if (xmlNode != null)
+            {
+                xmlNode.InnerText = newValue;
+            }
+        }
+
+        private XmlNode FindXmlNode(TreeNode treeNode, XmlNode parent) 
+        {
+            foreach (XmlNode node in parent.ChildNodes)
+            {
+                if (node.Name == treeNode.Text)
+                {
+                    return node;
+                }
+                XmlNode foundNode = FindXmlNode(treeNode, node);
+                if (foundNode != null)
+                {
+                    return foundNode;
+                }
+            }
+            return null;
+        }
+
+        public void ExportToXML(TreeView tree, string filename)
+        {
+            using (XmlTextWriter xmlTextWriter = new XmlTextWriter(filename, System.Text.Encoding.UTF8))
+            {
+                xmlTextWriter.Formatting = Formatting.Indented; 
+                xmlTextWriter.WriteStartDocument();
+                xmlTextWriter.WriteStartElement(tree.Nodes[0].Text);
+                foreach (TreeNode node in tree.Nodes)
+                {
+                    SaveToXML(node.Nodes, xmlTextWriter);
+                }
+                xmlTextWriter.WriteEndElement();
+                xmlTextWriter.Close();
+            }
+        }
+
+
+        private void SaveToXML(TreeNodeCollection nodes, XmlTextWriter writer)
+        {
+            foreach (TreeNode node in nodes)
+            {
+                writer.WriteStartElement("Node");
+                writer.WriteString(node.Text);
+                if (node.Nodes.Count > 0)
+                {
+                    SaveToXML(node.Nodes, writer);
+                }
+                writer.WriteEndElement();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dynamicTextBox = new TextBox
+            {
+                Location = new Point(325, 130),
+                Size = new Size(193, 20),
+                Text = ""
+            };
+            dynamicTextBox.KeyPress += TextBox_KeyPress;
+            dynamicTextBox.Focus();
+            this.Controls.Add(dynamicTextBox);
+
+            TreeNode newNode = new TreeNode(dynamicTextBox.Text);
+            if (treeView1.SelectedNode != null)
+            {
+                treeView1.SelectedNode.Nodes.Add(newNode);
+                treeView1.SelectedNode = newNode;
+            }
+            else
+            {
+                treeView1.Nodes.Add(newNode);
+                treeView1.SelectedNode = newNode;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (treeView1.SelectedNode != null)
+            {
+                if (treeView1.SelectedNode.Parent != null)
+                {
+                    treeView1.SelectedNode.Parent.Nodes.Remove(treeView1.SelectedNode);
+                }
+                else
+                {
+                    treeView1.Nodes.Remove(treeView1.SelectedNode);
+                }
+            }
+            else
+            {
+                MessageBox.Show("No node selected to delete.");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (doc != null)
+            {
+                try
+                {
+                    ExportToXML(treeView1, filePath);
+                    MessageBox.Show("Data saved successfully.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error saving XML: {ex.Message}");
+                }
+            }
+            else
+            {
+                MessageBox.Show("No data loaded to save.");
+            }
         }
     }
 }
